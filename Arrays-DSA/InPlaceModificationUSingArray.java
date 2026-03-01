06bb6abcd1469e3233b18f9f96c3ccc9e6dd9f9e
Yandapalli Tejaswini <reddytejaswini939@gmail.com>
2026-03-01 14:17:13 +0530

    Add files via upload

---
+++ Arrays-DSA/InPlaceModificationUSingArray.java
@@ -0,0 +1,15 @@
+//Time complexity:O(n),space complexity:O(1)
+
+public class InPlaceModificationUSingArray {
+    public static void main(String[] args) {
+        String s="hello world";
+        char arr[]=s.toCharArray();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==' '){
+                arr[i]='%';
+            }
+        }
+        String result=new String(arr);
+       System.out.println(result);
+    }
+}
