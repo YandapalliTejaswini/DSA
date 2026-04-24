d6b91c5414db711f2535aa52dbf09a762eb9fad1
Yandapalli Tejaswini <reddytejaswini939@gmail.com>
2026-04-24 17:59:10 +0530

    Add files via upload

---
+++ Arrays-DSA/EvenElements.java
@@ -0,0 +1,10 @@
+package Arrays;
+public class EvenElements {
+    public static void main(String[] args) {
+        int arr[]={0,1,2,3,4,5,6,7,8,9,10};
+        for(int i=0;i<arr.length;i+=2){
+          //  if(arr[i]%2==0)
+                System.out.println(arr[i]);
+        }
+    }
+}
